50c7d5a197a77f7d1f3f5cf15b2a6fc24eafd231
RENAITREIO <114514@example.com>
2025-08-05 16:11:26 +0800

    deleted

--- Embedded/stm32标准库/Hardware/LightSensor.h
+++
@@ -1,7 +0,0 @@
-#ifndef __LIGHTSENSOR_H
-#define __LIGHTSENSOR_H
-
-void LightSensor_Init(void);
-uint8_t LightSenor_GetState(void);
-
-#endif
--- Embedded/stm32标准库/System/MyDMA.h
+++
@@ -1,7 +0,0 @@
-#ifndef __MyDMA_H
-#define __MyDMA_H
-
-void MyDMA_Init(uint32_t AddrA, uint32_t AddrB, uint16_t Size);
-void MyDMA_Transfer(void);
-
-#endif
