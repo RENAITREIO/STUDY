f094905941a3d861ab26ab8a807ce584cfda8773
RENAITRE <2543563791@qq.com>
2026-05-10 15:50:50 +0800

    finish lab02

--- Lab/lab02/lfsr.c
+++ Lab/lab02/lfsr.c
@@ -4,7 +4,10 @@
 #include <string.h>
 #include "lfsr.h"
 
+#define GET_BIT(reg, n)    ((reg & (1 << n)) >> n)
+
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    uint16_t next = GET_BIT(*reg, 0) ^ GET_BIT(*reg, 2) ^ GET_BIT(*reg, 3) ^ GET_BIT(*reg, 5);
+    *reg = (next << 15) | (*reg >> 1);
 }
 
